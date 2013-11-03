6208d7899e88aa3622864dda6e81710f3b2bc45e
ohnorobo <sar.laplante@gmail.com>
2013-11-03 14:55:19 -0500

    adding titles to popups

--- js/script.js
+++ js/script.js
@@ -7,19 +7,69 @@ var createPopup = function(configs, features) {
     console.log("creating popup");
     var values = [];
     for(var key in configs){
-        if(features[key]){
+        if(features[key] && features[key] != "" && features[key].length != 0){
             values.push({
-                title: key,
-                value: features[key]
+                div_id: key,
+                value: formatElement(features[key], configs[key])
             })
         }
     }
     console.log(values);
-    console.log(popupTemplate({popup:values}));
-    return popupTemplate({popup:values});
-}
 
-var popupTemplate = Mustache.compile("<div>{{#popup}}<div id={{title}}>{{value}}</div id={{title}}>{{/popup}}</div>");
+    //Takes a list of html items and puts them in a popup template
+    var popupTemplate = Mustache.compile("<div>{{#popup}}<div id={{div_id}}>{{{value}}}</div id={{div_id}}>{{/popup}}</div>");
+    console.log(popupTemplate({popup:values}));
+
+    return popupTemplate({popup:values});
+};
+
+var formatElement = function(element, configs) {
+    console.log("format element", element);
+    if (configs['url']) {
+        console.log("url");
+        var title = configs['title'] || "[link]";
+        return urlTemplate(title, element);
+    }
+    if (configs['title']) {
+        console.log("title");
+        return titleTemplate(configs['title'], element);
+    }
+    if (Array.isArray(element)){
+        console.log("array");
+        return listTemplate(element);
+    }
+    console.log("simple");
+    return simpleTemplate(element);
+};
+
+var simpleTemplate = function(t){
+    console.log("simple template", t);
+    var template = Mustache.compile("{{text}}");
+    return template({"text":t});
+};
+var titleTemplate = function(title, text){
+    console.log("title template", title, text);
+    var template = Mustache.compile("<div> <h4>{{title}}</h4> <div>{{{text}}}</div> </div>");
+    return template({"title":title, "text":simpleTemplate(text)});
+};
+var listTemplate = function(elements){
+    console.log(elements);
+    var vals = [];
+    for (var i = 0; i < elements.length; i++){
+        var elem = elements[i];
+        console.log("list element", elem);
+        vals.push({"element":simpleTemplate(elem)})
+    }
+    console.log("list template", elements);
+    var template = Mustache.compile("<ul> {{#list}} <li>{{{element}}}</li> {{/list}} <ul>");
+    return template({"list":vals});
+};
+var urlTemplate = function(title, url){
+    console.log("url template", title, url);
+    var template = Mustache.compile("<a href={{url}}>{{title}}</a>");
+    return template({"title":title, "url":url});
+};
+
 
 console.log('loading config');
 L.Util.ajax('config.json').then(function(config){
@@ -38,7 +88,7 @@ L.Util.ajax('config.json').then(function(config){
     L.Util.ajax('data.geojson').then(function(data){
         //var layers = L.control.layers().addTo(map);
     
-        var features = function(feature, layer) {
+        var bindPopup = function(feature, layer) {
             console.log("running feature function");
             console.log(feature.properties.address);
             //console.log(layer);
@@ -48,14 +98,6 @@ L.Util.ajax('config.json').then(function(config){
         };
         console.log(data);
 
-        //layers.addOverlay(
-            L.geoJson(data,
-            {onEachFeature: features
-            //,
-            // filter:function(feature){
-            //    console.log("adding value -- " + feature.properties.address);
-            //    return typeof feature.properties !== 'undefined';
-            // }
-            }).addTo(map); //, "data");
+        L.geoJson(data, {onEachFeature: bindPopup}).addTo(map);
     });
 });
