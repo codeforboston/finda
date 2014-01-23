93be1471b6891c6e3ba006e874a93e6a223cec7f
Paul Swartz <paulswartz@gmail.com>
2014-01-22 22:00:20 -0500

    switch to using requirejs for dependencies

--- scripts/infotemplates.js
+++ scripts/infotemplates.js
@@ -1,79 +1,81 @@
-//setup values for the popup
-var createPopup = function(configs, features) {
-    //console.log("creating popup");
-    var values = [];
-    for(var key in configs){
-        if(features[key] && features[key] != "" && features[key].length != 0){
-            values.push({
-                div_id: key,
-                value: formatElement(features[key], configs[key])
-            })
+define(
+  ['handlebars'],
+  function(Handlebars) {
+    //setup values for the popup
+    var createPopup = function(configs, features) {
+      //console.log("creating popup");
+      var values = [];
+      for(var key in configs){
+        if(features[key] && features[key] !== "" &&
+           features[key].length !== 0){
+          values.push({
+            div_id: key,
+            value: formatElement(features[key], configs[key])
+          });
         }
-    }
-    //console.log(values);
+      }
+      //console.log(values);
 
-    //Takes a list of html items and puts them in a popup template
-    var popupTemplate = Handlebars.compile("<div>{{#popup}}<div id={{div_id}}>{{{value}}}</div id={{div_id}}>{{/popup}}</div>");
-    //console.log(popupTemplate({popup:values}));
+      //Takes a list of html items and puts them in a popup template
+      var popupTemplate = Handlebars.compile("<div>{{#popup}}<div id={{div_id}}>{{{value}}}</div id={{div_id}}>{{/popup}}</div>");
+      //console.log(popupTemplate({popup:values}));
 
-    return popupTemplate({popup:values});
-};
+      return popupTemplate({popup:values});
+    };
 
-
-var formatElement = function(element, configs) {
-    //console.log("format element", element);
-    if (configs['url']) {
+    var formatElement = function(element, configs) {
+      //console.log("format element", element);
+      if (configs.url) {
         //console.log("url");
-        var title = configs['title'] || "[link]";
+        var title = configs.title || "[link]";
         return urlTemplate(title, element, configs);
-    }
-    if (configs['title']) {
+      }
+      if (configs.title) {
         //console.log("title");
-        return titleTemplate(configs['title'], element, configs);
-    }
-    if (Array.isArray(element)){
+        return titleTemplate(configs.title, element, configs);
+      }
+      if (Array.isArray(element)){
         //console.log("array");
         return listTemplate(element, configs);
-    }
-    //console.log("simple");
-    return simpleTemplate(element, configs);
-};
+      }
+      //console.log("simple");
+      return simpleTemplate(element, configs);
+    };
 
+    var simpleTemplate = function(t){
+      //console.log("simple template", t);
+      t = t.replace(/\n/g, '<br />');
+      var template = Handlebars.compile("{{{text}}}");
+      return template({"text":t});
+    };
 
-var simpleTemplate = function(t, configs){
-    //console.log("simple template", t);
-    t = t.replace(/\n/g, '<br />');
-    var template = Handlebars.compile("{{{text}}}");
-    return template({"text":t});
-};
+    var titleTemplate = function(title, text){
+      //console.log("title template", title, text);
+      var template = Handlebars.compile("<div> <h4>{{title}}</h4> <div>{{{text}}}</div> </div>");
+      return template({"title":title, "text":formatElement(text, {})});
+    };
 
-
-var titleTemplate = function(title, text, configs){
-    //console.log("title template", title, text);
-    var template = Handlebars.compile("<div> <h4>{{title}}</h4> <div>{{{text}}}</div> </div>");
-    return template({"title":title, "text":formatElement(text, {})});
-};
-
-
-var listTemplate = function(elements, configs){
-    if (elements.length == 1) {
+    var listTemplate = function(elements, configs){
+      if (elements.length === 1) {
         return formatElement(elements[0], configs);
-    }
-    ///console.log(elements);
-    var vals = [];
-    for (var i = 0; i < elements.length; i++){
+      }
+      ///console.log(elements);
+      var vals = [];
+      for (var i = 0; i < elements.length; i++){
         var elem = elements[i];
         //console.log("list element", elem);
-        vals.push({"element":simpleTemplate(elem)})
-    }
-    //console.log("list template", elements);
-    var template = Handlebars.compile("<ul> {{#list}} <li>{{{element}}}</li> {{/list}} <ul>");
-    return template({"list":vals});
-};
+        vals.push({"element": simpleTemplate(elem)});
+      }
+      //console.log("list template", elements);
+      var template = Handlebars.compile("<ul> {{#list}} <li>{{{element}}}</li> {{/list}} <ul>");
+      return template({"list":vals});
+    };
 
+    var urlTemplate = function(title, url) {
+      //console.log("url template", title, url);
+      var template = Handlebars.compile("<a href={{url}}>{{title}}</a>");
+      return template({"title":title, "url":url});
+    };
 
-var urlTemplate = function(title, url, configs){
-    //console.log("url template", title, url);
-    var template = Handlebars.compile("<a href={{url}}>{{title}}</a>");
-    return template({"title":title, "url":url});
-};
+    return createPopup;
+  });
--- scripts/script.js
+++ scripts/script.js
@@ -1,121 +1,150 @@
 'use strict';
-
-// define icon styles
-var grayIcon = L.icon({
-    iconUrl: "leaflet/images/marker-icon-gray.png"   
+require.config({
+  baseUrl: "scripts/",
+  paths: {
+    "jquery": "dependencies/jquery-1.10.2",
+    "leaflet": "../leaflet/leaflet",
+    "handlebars": "dependencies/handlebars",
+    "leaflet-search": "../leaflet/leaflet-search",
+    "leaflet.control.geosearch": "../leaflet/l.control.geosearch",
+    "leaflet.geosearch.provider.google": "../leaflet/l.geosearch.provider.google"
+  },
+  shim: {
+    'handlebars': {
+      exports: 'Handlebars'
+    },
+    'leaflet.geosearch': {
+      exports: 'L'
+    },
+    'leaflet.geosearch.provider.google': {
+      deps: ['leaflet.control.geosearch'],
+      exports: 'L'
+    }
+  }
 });
 
-var defaultIcon = L.icon({
-    iconUrl: "leaflet/images/marker-icon.png"   
-});
+define(
+  ['leaflet',
+   'infotemplates',
+   'jquery',
+   'leaflet-search',
+   'leaflet.geosearch.provider.google'],
+  function(L, createPopup, $) {
+    // define icon styles
+    L.Icon.Default.imagePath = '/leaflet/images/';
+    var grayIcon = L.icon({
+      iconUrl: "leaflet/images/marker-icon-gray.png"
+    });
 
-console.log('loading config');
-L.Util.ajax('config.json').then(function(config){
-    var map = makeBaseMap(config.map);
+    var defaultIcon = L.icon({
+      iconUrl: "leaflet/images/marker-icon.png"
+    });
 
-    console.log('loading geojson');
-    L.Util.ajax('data.geojson').then(function(data){
+    /*
+     var makeFacetBox = function(config, map, data) {
+     facetCounts = {};
+
+     data.options.onEachFeature(function(feature, layer){
+
+     });
+
+     var facet = L.control();
+     facet.setPosition("bottomleft");
+
+     facet.onAdd = function () {
+     return this._div = L.DomUtil.create('div', 'facet'); // create a div with a class "info"
+     };
+
+     facet.addTo(map);
+
+     return facet;
+     }
+     */
+
+    var makeGeoSearch = function(map){
+      var geosearch = new L.Control.GeoSearch({
+        provider: new L.GeoSearch.Provider.Google(),
+        position: 'topleft',
+        showMarker: false
+      });
+
+      geosearch.addTo(map);
+
+      return geosearch;
+    };
+
+    var makeInfoBox = function(config, map){
+      var info = L.control();
+
+      info.onAdd = function () {
+        this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+        return this._div;
+      };
+
+      info.update = function (featureProps) {
+        this._div.innerHTML = createPopup(config, featureProps);
+      };
+
+      info.addTo(map);
+      return info;
+    };
+
+    var makeBaseMap = function(config){
+      var map = L.map('map', {zoomControl: false});
+
+      if (config){
+        if (!location.hash) {
+          map.setView(config.center, config.zoom);
+        }
+        if (config.maxZoom){
+          map.options.maxZoom = config.maxZoom;
+        }
+        if (config.maxBounds){
+          map.setMaxBounds(config.maxBounds);
+        }
+      }
+      //map.addHash();
+
+      L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png', {
+        attribution: 'Tiles Courtesy of <a href="http://mapc.org">MAPC</a> &mdash; Map data &copy; <a href="http://www.mass.gov/mgis/">MassGIS</a>',
+        subdomains: '1234'
+      }).addTo(map);
+
+      return map;
+    };
+
+    $.getJSON("config.json", function(config) {
+      var map = makeBaseMap(config.map);
+      $.getJSON("data.geojson", function(data) {
 
         var info = makeInfoBox(config.properties, map);
-        var search = makeGeoSearch(map);
+        makeGeoSearch(map);
 
         var previouslyClicked;
 
         var onClick = function(e){
-            map.panTo(e.latlng); //zoom to the clicked element
-            info.update(e.target.feature.properties); //update infobox
-            this.setIcon(grayIcon); // set the clicked marker to be gray
+          map.panTo(e.latlng); //zoom to the clicked element
+          info.update(e.target.feature.properties); //update infobox
+          this.setIcon(grayIcon); // set the clicked marker to be gray
 
-            // reset the previously clicked marker
-            if(previouslyClicked){
-                previouslyClicked.setIcon(defaultIcon); 
-            }
+          // reset the previously clicked marker
+          if(previouslyClicked){
+            previouslyClicked.setIcon(defaultIcon);
+          }
 
-            previouslyClicked = this;
+          previouslyClicked = this;
         };
 
         var setupFeature = function(feature, layer) {
-            console.log(feature.properties.address);
-            layer.on({ click: onClick });
+          console.log(feature.properties.address);
+          layer.on({ click: onClick });
         };
 
-        var geoLayer = L.geoJson(data, {onEachFeature: setupFeature}).addTo(map);
+        L.geoJson(data, {onEachFeature: setupFeature}).addTo(map);
         //var facet = makeFacetBox(config.facets, map, geoLayer)
 
+      });
+
+      map.locate({setView: true, maxZoom: 14}); //geolocate on load
     });
-
-    map.locate({setView: true, maxZoom: 14}); //geolocate on load
-});
-
-/*
-var makeFacetBox = function(config, map, data) {
-    facetCounts = {};
-
-    data.options.onEachFeature(function(feature, layer){
-        
-    });
-
-    var facet = L.control();
-    facet.setPosition("bottomleft");
-
-    facet.onAdd = function () {
-        return this._div = L.DomUtil.create('div', 'facet'); // create a div with a class "info"
-    };
-
-    facet.addTo(map);
-
-    return facet;
-}
-*/
-
-
-var makeGeoSearch = function(map){
-    var geosearch = new L.Control.GeoSearch({
-        provider: new L.GeoSearch.Provider.Google(),
-        position: 'topleft',
-        showMarker: false
-    })
-
-    geosearch.addTo(map);
-
-    return geosearch;
-}
-
-var makeInfoBox = function(config, map){
-    var info = L.control();
-
-    info.onAdd = function () {
-        return this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-    };
-
-    info.update = function (featureProps) {
-        this._div.innerHTML = createPopup(config, featureProps);
-    };
-
-    info.addTo(map);
-    return info
-}
-
-var makeBaseMap = function(config){
-    var map = L.map('map', {zoomControl: false});
-
-    if (config){
-        if (!location.hash) {
-            map.setView(config.center, config.zoom);
-        }
-        if (config.maxZoom){
-            map.options.maxZoom = config.maxZoom;
-        }
-        if (config.maxBounds){
-            map.setMaxBounds(config.maxBounds);
-        }
-    }
-    //map.addHash();
-
-    var mq = L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png', {        
-        attribution: 'Tiles Courtesy of <a href="http://mapc.org">MAPC</a> &mdash; Map data &copy; <a href="http://www.mass.gov/mgis/">MassGIS</a>',
-        subdomains: '1234'
-    }).addTo(map);
-
-    return map;
-}
+  });
