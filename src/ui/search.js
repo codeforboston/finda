65dd20884b2a07839b7ee80791982d919a39c05d
Paul Swartz <paulswartz@gmail.com>
2014-03-04 20:36:03 -0500

    Switch to the OSM Geocoder
    Closes #52

--- src/ui/search.js
+++ src/ui/search.js
@@ -1,59 +1,57 @@
-/*global google*/
 define(
   ['flight',
-   'jquery'],
-  function(flight, $) {
+   'jquery',
+   'lodash'],
+  function(flight, $, _) {
     'use strict';
     var search = function() {
       this.defaultAttrs({
-        searchSelector: 'input'
+        searchSelector: 'input',
+        searchUrl: '//nominatim.openstreetmap.org/search'
       });
       this.configureSearch = function(ev, config) {
         if (config.search && config.search.geosearch) {
-          window.googleMapsApiLoaded = this.googleMapsApiLoaded.bind(this);
           this.maxBounds = config.map.maxBounds;
-          this.addGoogleScript();
+          this.$node.show();
         } else {
           this.$node.hide();
         }
       };
 
-      this.addGoogleScript = function() {
-        // separate function so we can mock it for testing
-        this.script = $('<script />')
-          .attr('type', 'text/javascript')
-            .attr('src', '//maps.google.com/maps/api/js?sensor=false&callback=googleMapsApiLoaded');
-        this.script.appendTo('head');
-      };
-
-      this.googleMapsApiLoaded = function() {
-        this.script.remove();
-        this.geocoder = new google.maps.Geocoder();
-        this.$node.show();
-      };
-
       this.search = function(ev) {
         ev.preventDefault();
         var address = this.select('searchSelector').val(),
-            parameters = {address: address};
+            parameters = {
+              format: "json",
+              addressdetails: 1,
+              q: address
+            };
         if (!address) {
           return;
         }
         if (this.maxBounds) {
-          parameters.bounds = new google.maps.LatLngBounds(
-            new google.maps.LatLng(this.maxBounds[0][0], this.maxBounds[0][1]),
-            new google.maps.LatLng(this.maxBounds[1][0], this.maxBounds[1][1]));
+          parameters.viewbox = [
+            this.maxBounds[0][0], this.maxBounds[0][1],
+            this.maxBounds[1][0], this.maxBounds[1][1]].join(',');
         }
-        this.geocoder.geocode(parameters, this.searchResults.bind(this));
+        $.getJSON(this.attr.searchUrl,
+                  parameters,
+                  this.searchResults.bind(this));
       };
 
       this.searchResults = function(results) {
         if (results.length) {
-          var location = results[0].geometry.location;
+          var location = results[0],
+              displayName = _.compact([
+                location.address.road,
+                location.address.city,
+                location.address.state]).join(', ');
+          this.select('searchSelector').attr('placeholder',
+                                             displayName).val('');
           this.trigger(this.attr.mapSelector,
                        'panTo',
-                       {lat: location.lat(),
-                        lng: location.lng()});
+                       {lat: location.lat,
+                        lng: location.lon});
         }
       };
 
