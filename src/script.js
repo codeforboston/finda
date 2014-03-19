7238c4f1dbc8870d82dc29c5ee08265a3974f7f9
Calvin Metcalf <cmetcalf@appgeo.com>
2014-03-18 20:57:16 -0400

    move config

--- src/script.js
+++ src/script.js
@@ -1,3 +1,31 @@
+require.config({
+    baseUrl: 'src/',
+    paths: {
+      'jquery': '../lib/jquery-1.10.2',
+      'bootstrap': '../lib/bootstrap.min',
+      'leaflet': '../lib/leaflet/leaflet',
+      'handlebars': '../lib/handlebars',
+      'lodash': '../lib/lodash.min',
+      'flight': '../lib/flight.min'
+    },
+    shim: {
+      'handlebars': {
+        exports: 'Handlebars'
+      },
+      'underscore': {
+        exports: '_'
+      },
+      'flight': {
+        deps: ['../lib/es5-shim.min', '../lib/es5-sham.min'],
+        exports: 'flight'
+      },
+      'bootstrap': {
+        deps: ['jquery'],
+        exports: '$'
+      }
+    }
+});
+
 define(function(require) {
     'use strict';
     require('bootstrap');
