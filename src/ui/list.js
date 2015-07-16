c00e0784006c67f12220321c8c3d11c8b06e80f5
Nick Jones <nick@tinygoatstudio.com>
2015-07-15 20:35:07 -0400

    Removes data directory

--- src/ui/list.js
+++ src/ui/list.js
@@ -78,6 +78,7 @@ define(function(require, exports, module) {
       var feature = $li.data('feature');
       this.trigger('selectFeature', feature);
       $('.selected').removeClass('selected');
+      $('.selected').removeClass('selected');
       $li.addClass('selected');
     };
 
