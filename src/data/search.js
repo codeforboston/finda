2d38da58e9be848e42c86af7e5c2cee68cd45b60
Jason Marshall <marshallj88@gmail.com>
2015-08-08 20:10:40 -0400

    push head updates

--- src/data/search.js
+++ src/data/search.js
@@ -19,16 +19,10 @@ define(function(require) {
     this.onSearch = function(ev, options) {
       ev.preventDefault();
       var parameters = {
-<<<<<<< HEAD
         // addressdetails: 1,
         format: "json",
         q: options.query,
         bounded: 1
-=======
-        format: "json",
-        Addressdetails: 1,
-        q: options.query
->>>>>>> Beginning of search fix
       };
       if (this.maxBounds) {
         parameters.viewbox = [
