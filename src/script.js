32d26911ad8ec402dcc2921a117819c59d655d78
Erik Schwartz <erik@erikschwartz.net>
2015-08-28 12:33:43 -0400

    Re-enable map

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ define(function(require) {
   'use strict';
   require('bootstrap');
   // attach components to the DOM
-  // require('ui/map').attachTo('#map');
+  require('ui/map').attachTo('#map');
   require('ui/search').attachTo('#search');
   require('ui/search_results').attachTo('#search-results');
   require('ui/info').attachTo('#info');
