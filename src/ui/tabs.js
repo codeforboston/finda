e1045c4a0cf373c5637a1375a7623e060e740237
Erik Schwartz <erik@erikschwartz.net>
2015-10-26 11:11:17 -0400

    Remove survey-tab class on results tab click

--- src/ui/tabs.js
+++ src/ui/tabs.js
@@ -3,11 +3,24 @@ define(function(require, exports, module) {
   var flight = require('flight');
 
   module.exports = flight.component(function sidebar() {
-    this.after('initialize', function() {
-      this.on(document, 'uiShowResults', function() {
+    this.onShowResults = function(event, opts) {
+      if (!opts) { opts = {}; }
+
+      if (!opts.dontClickTab) {
         this.$node.find('#results-tab').click();
-        this.$node.find('.survey-tabs').removeClass('survey-tabs');
-      });
+      }
+      this.$node.find('.survey-tabs').removeClass('survey-tabs');
+    };
+
+    this.setupClickHandlers = function() {
+      this.$node.find('#results-tab').on('click', function() {
+        this.trigger('uiShowResults', {dontClickTab: true});
+      }.bind(this));
+    };
+
+    this.after('initialize', function() {
+      this.on(document, 'uiShowResults', this.onShowResults);
+      this.setupClickHandlers();
     });
   });
 });
