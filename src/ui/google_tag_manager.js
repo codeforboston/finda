6518234904889b2d532f10139585638295e04f7f
Erik Schwartz <erik@erikschwartz.net>
2016-01-30 10:34:43 -0500

    Send feedback through Google Docs, add disclaimer

--- src/ui/google_tag_manager.js
+++ src/ui/google_tag_manager.js
@@ -1,21 +1,49 @@
 define(function(require, exports, module) {
   'use strict';
   var flight = require('flight');
+  var $ = require('jquery');
 
   module.exports = flight.component(function analytics() {
+    this.feedback = function() {
+      return this.$node.find('#feedback-text').val();
+    };
+
+    this.submitBtn = function() {
+      return this.$node.find('.js-submit');
+    };
+
+    this.resetSubmitBtn = function() {
+      this.submitBtn().html('Send');
+    };
+
     this.handleSubmission = function(e) {
       e.preventDefault();
-      window.dataLayer.push({
-        'eventLabel': this.$node.find('#feedback-text').val()
+      this.submitBtn().html('Sending...');
+
+      // based on https://mashe.hawksey.info/2014/07/google-sheets-as-a-database-insert-with-apps-script-using-postget-methods-with-ajax-example/
+      $.ajax({
+        url: 'https://script.google.com/macros/s/AKfycbzziKocYO7ZmbLvRaSI_OEFSHTVwnCFrTfQT-OzoqAVQvpg1ZE/exec',
+        type: "post",
+        data: {"feedback": this.feedback() },
+        success: this.success.bind(this),
+        error: this.error.bind(this)
       });
-      this.success();
+    };
+
+    this.error = function(data) {
+      this.resetSubmitBtn();
+      var error = this.$node.find('.js-error');
+      var body = this.feedback() + " (Error details: " + data.responseText + ")";
+      error.find('.js-error-email').prop('href', 'mailto:gethelplex@lexingtonky.gov?subject=[GetHelpLex feedback]&body=' + body);
+      error.show();
     };
 
     this.success = function() {
-      this.$node.find('.thank-you').show();
+      this.resetSubmitBtn();
+      this.$node.find('.js-thank-you').show();
       setTimeout(function() {
         this.$node.modal('hide');
-        this.$node.find('.thank-you').hide();
+        this.$node.find('.js-thank-you').hide();
       }.bind(this), 2000);
     };
 
