b3877976406be64c4bd9a808137bb5eac571cfc1
ankurMalhotra <ankur.malhotra7@ymail.com>
2016-04-13 11:20:38 +1200

    Fix errors

--- src/ui/back-to-top.js
+++ src/ui/back-to-top.js
@@ -1,16 +1,17 @@
-define(function (require, exports, module) {
-    'use strict';
-    var flight = require('flight');
+define(function(require, exports, module) {
+  'use strict';
+  var flight = require('flight');
+  var $ = require('jquery');
 
-    module.exports = flight.component(function () {
-        this.onClick = function () {
-            $('body,html').animate({
-                scrollTop: 0
-            }, 500);
-        };
+  module.exports = flight.component(function() {
+    this.onClick = function() {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 500);
+    };
 
-        this.after('initialize', function () {
-            this.on('click', this.onClick);
-        });
+    this.after('initialize', function() {
+      this.on('click', this.onClick);
     });
+  });
 });
