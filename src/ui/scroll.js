6bbe1a7a67aa0e5e9d451e64123c610d64c13cdf
ankurMalhotra <ankur.malhotra7@ymail.com>
2016-04-13 11:18:08 +1200

    fix indentation

--- src/ui/scroll.js
+++ src/ui/scroll.js
@@ -1,19 +1,20 @@
-define(function (require, exports, module) {
-    'use strict';
-    var flight = require('flight');
+define(function(require, exports, module) {
+  'use strict';
+  var flight = require('flight');
+  var $ = require('jquery');
 
-    module.exports = flight.component(function () {
+  module.exports = flight.component(function() {
 
-        this.onScroll = function () {
-            if (this.$node.scrollTop() >= 500) {
-                $('#back-to-top').fadeIn(200);
-            } else {
-                $('#back-to-top').fadeOut(200);
-            }
-        }
+    this.onScroll = function() {
+      if (this.$node.scrollTop() >= 500) {
+        $('#back-to-top').fadeIn(200);
+      } else {
+        $('#back-to-top').fadeOut(200);
+      }
+    };
 
-        this.after('initialize', function () {
-            this.on('scroll', this.onScroll);
-        });
+    this.after('initialize', function() {
+      this.on('scroll', this.onScroll);
     });
+  });
 });
