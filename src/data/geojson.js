aaa04276af96dc1c12e6c41ed4be214fe8936ffb
Ron Northrip <ron.northrip@gmail.com>
2017-03-16 15:18:37 -0400

    Added assessment services to facility types too

--- src/data/geojson.js
+++ src/data/geojson.js
@@ -58,6 +58,7 @@ define(function(require, exports, module) {
         outpatient_offered: "facility_type",
         residential_offered: "facility_type",
         medical_detox_offered: "facility_type",
+        assessment_offered: "facility_type",
         outpatient_intensive: "out_patient",
         outpatient_services: "out_patient",
         outpatient_mat: "out_patient",
