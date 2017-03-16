34146889b63651693ebf656af075de696de8a8fd
Ron Northrip <ron.northrip@gmail.com>
2017-03-16 09:27:09 -0400

    Add web_url property assignment to getjson row reader

--- src/data/geojson.js
+++ src/data/geojson.js
@@ -27,7 +27,8 @@ define(function(require, exports, module) {
         "phone_numbers": csvRow.phone_numbers,
         "address": csvRow.address + " " + csvRow.city + ", Kentucky",
         "city": csvRow.city,
-        "county": csvRow.county
+        "county": csvRow.county,
+        "web_url": csvRow.web_url
       };
 
       _.each(facetValues, function(facet, facetValue) {
